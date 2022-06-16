75a3e16edc4d31b3832f1d11a1fb72c1c46e9f19
Adrian Coropco <adi_coropco@yahoo.com>
2022-06-16 12:45:26 +0300

    Add AppSettings and create extension for service collection

---
+++ FestivalApp/FestivalApp.API/Extensions/FestivalServicesExtensions.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using FestivalApp.API.ExceptionFilter;
+using FestivalApp.API.MappingProfiles;
+using FestivalApp.Core.Helpers;
+using FestivalApp.Core.Interfaces;
+using FestivalApp.Core.Models;
+using FestivalApp.Core.Providers;
+using FestivalApp.Domain.Entities;
+using FestivalApp.Infrastructure.Data;
+using FestivalApp.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using IQueryProvider = FestivalApp.Core.Interfaces.IQueryProvider;
+
+namespace FestivalApp.API.Extensions
+{
+    public static class FestivalServicesExtensions
+    {
+        public static void SetIdentityDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            IdentityBuilder builder = services.AddIdentityCore<User>(option =>
+            {
+                option.Password.RequireDigit = false;
+                option.Password.RequiredLength = 4;
+                option.Password.RequireNonAlphanumeric = false;
+                option.Password.RequireUppercase = false;
+                option.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddDefaultTokenProviders();
+
+            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+            builder.AddEntityFrameworkStores<DataContext>();
+            builder.AddRoleValidator<RoleValidator<Role>>();
+            builder.AddRoleManager<RoleManager<Role>>();
+            builder.AddSignInManager<SignInManager<User>>();
+
+            services.Configure<DataProtectionTokenProviderOptions>(opt => opt.TokenLifespan = TimeSpan.FromHours(2));
+
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                        .GetBytes(configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+            });
+
+            services.AddControllers(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                  .RequireAuthenticatedUser()
+                  .Build();
+
+                options.Filters.Add(new AuthorizeFilter(policy));
+            })
+            .AddNewtonsoftJson(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+        }
+
+        public static void SetFestivalServicesDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Email Sender configuration
+            var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
+
+            // AutoMapper configuration
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperProfiles());
+            });
+            var mapper = mapperConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+            // Register ExceptionFilter
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add<GlobalExceptionFilter>();
+            });
+
+            // Register MediatR and its providers
+            services.AddMediatR(typeof(Startup));
+            services.AddTransient<ICommandProvider, CommandProvider>();
+            services.AddTransient<IQueryProvider, QueryProvider>();
+
+            services.AddScoped<IFestivalRepository, FestivalRepository>();
+        }
+    }
+}
--- FestivalApp/FestivalApp.API/Startup.cs
+++ FestivalApp/FestivalApp.API/Startup.cs
@@ -1,25 +1,6 @@
-﻿using System.Text;
-using AutoMapper;
-using FestivalApp.API.MappingProfiles;
-using FestivalApp.API.ExceptionFilter;
-using FestivalApp.Core.Helpers;
-using FestivalApp.Core.Interfaces;
-using FestivalApp.Core.Models;
-using FestivalApp.Domain.Entities;
+﻿using FestivalApp.API.Extensions;
 using FestivalApp.Infrastructure.Data;
-using FestivalApp.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.IdentityModel.Tokens;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using FestivalApp.Core.Providers;
-using IQueryProvider = FestivalApp.Core.Interfaces.IQueryProvider;
 
 namespace FestivalApp.API
 {
@@ -46,87 +27,13 @@ namespace FestivalApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var emailConfig = Configuration
-              .GetSection("EmailConfiguration")
-              .Get<EmailConfiguration>();
-            services.AddSingleton(emailConfig);
-            services.AddScoped<IEmailSender, EmailSender>();
+            // Register Identity Services
+            services.SetIdentityDependencies(Configuration);
 
-            IdentityBuilder builder = services.AddIdentityCore<UserModel>(option =>
-            {
-                option.Password.RequireDigit = false;
-                option.Password.RequiredLength = 4;
-                option.Password.RequireNonAlphanumeric = false;
-                option.Password.RequireUppercase = false;
-                option.SignIn.RequireConfirmedEmail = true;
-            })
-            .AddDefaultTokenProviders();
-
-            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
-            builder.AddEntityFrameworkStores<DataContext>();
-            builder.AddRoleValidator<RoleValidator<Role>>();
-            builder.AddRoleManager<RoleManager<Role>>();
-            builder.AddSignInManager<SignInManager<UserModel>>();
-
-            services.Configure<DataProtectionTokenProviderOptions>(opt => opt.TokenLifespan = TimeSpan.FromHours(2));
-
-            services.TryAddSingleton<ISystemClock, SystemClock>();
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddJwtBearer(options =>
-              {
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuerSigningKey = true,
-                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                      ValidateIssuer = false,
-                      ValidateAudience = false
-                  };
-              });
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-            });
-
-            services.AddControllers(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                  .RequireAuthenticatedUser()
-                  .Build();
-
-                options.Filters.Add(new AuthorizeFilter(policy));
-            })
-              .AddNewtonsoftJson(opt =>
-              {
-                  opt.SerializerSettings.ReferenceLoopHandling =
-              Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-              });
+            // Register Festival Services
+            services.SetFestivalServicesDependencies(Configuration);
 
             services.AddCors();
-
-            // AutoMapper configuration
-            var mapperConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new AutoMapperProfiles());
-            });
-            var mapper = mapperConfig.CreateMapper();
-
-            services.AddSingleton(mapper);
-
-            // Register ExceptionFilter
-            services.Configure<MvcOptions>(options =>
-            {
-                options.Filters.Add<GlobalExceptionFilter>();
-            });
-
-            // Register MediatR and its providers
-            services.AddMediatR(typeof(Startup));
-            services.AddTransient<ICommandProvider, CommandProvider>();
-            services.AddTransient<IQueryProvider, QueryProvider>();
-
-            services.AddScoped<IFestivalRepository, FestivalRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
